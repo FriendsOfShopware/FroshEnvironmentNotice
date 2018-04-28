5699b97121aaa790875bbfbaec7b734a4bc87686
Joshua Behrens <joshua.behrens@niemand.online>
2018-04-28 07:01:17 +0200

    Wrapped message editor in tabs

--- Resources/notice-editor/src/App.vue
+++ Resources/notice-editor/src/App.vue
@@ -1,78 +1,82 @@
 <template>
-  <div id="app" class="container-fluid py-5">
-    <div class="w-100 sticky-top position-fixed px-4 mt-5">
-      <b-alert v-for="alert in alerts" v-bind:variant="alert.variant" dismissible show>
-        {{alert.message}}
-        <template slot="dismiss">
-          &times;
-        </template>
-      </b-alert>
-    </div>
-    <b-navbar fixed="top" class="navbar-light bg-light">
-      <b-nav-form>
-        <b-button variant="outline-success" size="sm" v-on:click="addNotice" v-bind:disabled="isLoading">
-          Add
-        </b-button>
-      </b-nav-form>
-    </b-navbar>
-    <b-table v-bind:items="notices" v-bind:fields="fields" v-bind:busy="isLoading" hover small>
-      <template slot="actions" slot-scope="row">
-        <b-button-group size="sm">
-          <b-button variant="outline-secondary" v-on:click.stop="row.toggleDetails" v-bind:pressed="row.detailsShowing" v-if="row.item.id">
-            Edit
-          </b-button>
-          <b-button variant="outline-danger" v-on:click="deleteNotice(row.item)" v-if="row.item.id">
-            Delete
-          </b-button>
-          <b-button variant="outline-danger" v-on:click="cancelNotice(row.item)" v-else>
-            Cancel
-          </b-button>
-        </b-button-group>
-      </template>
-      <template slot="row-details" slot-scope="row">
-        <b-card>
-          <b-form-group id="fieldName"
-                        label="Name"
-                        label-for="inputName"
-                        v-bind:label-cols="3"
-                        horizontal>
-            <b-form-input id="inputName" v-model="row.item.name"></b-form-input>
-          </b-form-group>
-          <b-form-group id="fieldMessage"
-                        v-bind:label-cols="3"
-                        label="Message"
-                        label-for="inputMessage"
-                        horizontal>
-            <b-form-input id="inputMessage" v-model="row.item.message"></b-form-input>
-          </b-form-group>
-          <b-row>
-            <b-col class="text-right">
+  <div id="app">
+    <b-card no-body>
+      <b-tabs card>
+        <b-tab title="Messages" active>
+          <div class="w-100 sticky-top position-fixed px-4 mt-5">
+            <b-alert v-for="alert in alerts" v-bind:variant="alert.variant" dismissible show>
+              {{alert.message}}
+              <template slot="dismiss">
+                &times;
+              </template>
+            </b-alert>
+          </div>
+          <b-navbar class="navbar-light bg-light">
+            <b-nav-form>
+              <b-button variant="outline-success" size="sm" v-on:click="addNotice" v-bind:disabled="isLoading">
+                Add
+              </b-button>
+            </b-nav-form>
+            <b-nav-form right>
+              <b-button variant="outline-primary" size="sm" v-on:click="loadData" v-bind:disabled="isLoading">
+                Load
+              </b-button>
+            </b-nav-form>
+          </b-navbar>
+          <b-table v-bind:items="notices" v-bind:fields="fields" v-bind:busy="isLoading" hover small>
+            <template slot="actions" slot-scope="row">
               <b-button-group size="sm">
-                <b-button variant="outline-success" v-on:click="saveNotice(row.item)" v-if="row.item.id">
-                  Save
+                <b-button variant="outline-secondary" v-on:click.stop="row.toggleDetails" v-bind:pressed="row.detailsShowing" v-if="row.item.id">
+                  Edit
                 </b-button>
-                <b-button variant="outline-success" v-on:click="insertNotice(row.item)" v-else>
-                  Add
-                </b-button>
-                <b-button variant="outline-danger" v-on:click="resetNotice(row.item)" v-if="row.item.id">
-                  Cancel
+                <b-button variant="outline-danger" v-on:click="deleteNotice(row.item)" v-if="row.item.id">
+                  Delete
                 </b-button>
                 <b-button variant="outline-danger" v-on:click="cancelNotice(row.item)" v-else>
                   Cancel
                 </b-button>
               </b-button-group>
-            </b-col>
-          </b-row>
-        </b-card>
-      </template>
-    </b-table>
-    <b-navbar fixed="bottom" class="navbar-light bg-light">
-      <b-nav-form>
-        <b-button variant="outline-primary" size="sm" v-on:click="loadData" v-bind:disabled="isLoading">
-          Load
-        </b-button>
-      </b-nav-form>
-    </b-navbar>
+            </template>
+            <template slot="row-details" slot-scope="row">
+              <b-card>
+                <b-form-group id="fieldName"
+                              label="Name"
+                              label-for="inputName"
+                              v-bind:label-cols="3"
+                              horizontal>
+                  <b-form-input id="inputName" v-model="row.item.name"></b-form-input>
+                </b-form-group>
+                <b-form-group id="fieldMessage"
+                              v-bind:label-cols="3"
+                              label="Message"
+                              label-for="inputMessage"
+                              horizontal>
+                  <b-form-input id="inputMessage" v-model="row.item.message"></b-form-input>
+                </b-form-group>
+                <b-row>
+                  <b-col class="text-right">
+                    <b-button-group size="sm">
+                      <b-button variant="outline-success" v-on:click="saveNotice(row.item)" v-if="row.item.id">
+                        Save
+                      </b-button>
+                      <b-button variant="outline-success" v-on:click="insertNotice(row.item)" v-else>
+                        Add
+                      </b-button>
+                      <b-button variant="outline-danger" v-on:click="resetNotice(row.item)" v-if="row.item.id">
+                        Cancel
+                      </b-button>
+                      <b-button variant="outline-danger" v-on:click="cancelNotice(row.item)" v-else>
+                        Cancel
+                      </b-button>
+                    </b-button-group>
+                  </b-col>
+                </b-row>
+              </b-card>
+            </template>
+          </b-table>
+        </b-tab>
+      </b-tabs>
+    </b-card>
   </div>
 </template>
 
@@ -179,5 +183,17 @@ export default {
   td.table-col-minimum,
   th.table-col-minimum {
     width: 1%;
+  }
+
+  #app .card-body {
+    padding: 0;
+  }
+
+  body,
+  body > #app,
+  body > #app > .card {
+    height: 100vh;
+    border: unset;
+    border-radius: 0;
   }
 </style>
